03893ed3bb17230e410d5ba8427641ba165c28d0
coltenappleby <colten.appleby@gmail.com>
2021-04-09 16:44:08 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+function countDown(num) {
+
+    for ( num; num >= 0; num-- ) {
+        console.log(num)
+    }
+}
+
+
+function writeCards(names, giftType) {
+    let thankYouMessages = []
+    for ( let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the wonderful ${giftType} gift!`)
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${giftType} gift!`)
+    }
+    return thankYouMessages
+}
+
