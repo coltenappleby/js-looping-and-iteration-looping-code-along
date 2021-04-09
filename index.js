b7ab9ac752569f2bca7dc1797488303c6a40e6e8
coltenappleby <colten.appleby@gmail.com>
2021-04-09 16:43:43 -0400

    commit tree 9118fcf67b46b412793f3180fcf4f85f3dbce8ef

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+function countDown(num) {
+
+    for ( num; num >= 0; num-- ) {
+        console.log(num)
+    }
+}
+
+
+function writeCards(names, giftType) {
+    let tempArry = []
+    for ( let i = 0; i < names.length; i++) {
+        console.log(`Thank you, ${names[i]}, for the wonderful ${giftType} gift!`)
+        tempArry.push(`Thank you, ${names[i]}, for the wonderful ${giftType} gift!`)
+    }
+    return tempArry
+}
+
